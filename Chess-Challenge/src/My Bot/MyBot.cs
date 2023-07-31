77bb10067df4f7955dbddbfee0b72e7b6e5d6ec4
KingKnecht <sven.bardos@googlemail.com>
2023-07-31 16:48:23 +0200

    vor Umbau

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,17 +12,20 @@ public class MyBot : IChessBot
     private bool _playerIsWhite;
     private int _moveCount = 0;
     private int _resultsCalculated;
+    private Random _rnd;
 
     public Move Think(Board board, Timer timer)
     {
+        _rnd = new Random(DateTime.Now.Millisecond);
+        
         _resultsCalculated = 0;
 
-        int maxDepth = 8;
+        int maxDepth = 6;
 
         int depth = maxDepth - 1;
         _playerIsWhite = board.IsWhiteToMove;
 
-        var result = AlphaBeta(board, depth, maxDepth, true, board.IsWhiteToMove, float.MinValue, float.MaxValue, Move.NullMove);
+        var result = AlphaBeta(board, depth, maxDepth, true, board.IsWhiteToMove, float.MinValue, float.MaxValue);
 
         Console.WriteLine($"Move #{++_moveCount}");
         Console.WriteLine($"Best {_bestMove}");
@@ -36,7 +39,7 @@ public class MyBot : IChessBot
     }
 
 
-    private int AlphaBeta(Board board, int depth, int maxDepth, bool isMaximizer, bool isWhite, float alpha, float beta, Move lastMove)
+    private int AlphaBeta(Board board, int depth, int maxDepth, bool isMaximizer, bool isWhite, float alpha, float beta)
     {
 
         // If max depth is reached or Game is Over
@@ -48,12 +51,24 @@ public class MyBot : IChessBot
         }
 
         var orderedMoves = board.GetLegalMoves()
-                .OrderByDescending(m => MoveValue(m.MovePieceType, m.CapturePieceType))
+                .OrderByDescending(m =>
+                {
+                    board.MakeMove(m);
+                    var isInCheck = board.IsInCheck();
+                    board.UndoMove(m);
+
+                    return isInCheck;
+                })
+                .ThenByDescending(m => MoveValue(m.MovePieceType, m.CapturePieceType))
                 .ThenByDescending(m => m.IsPromotion)
                 .ThenByDescending(m => m.IsCastles)
-                .ThenByDescending(m =>
-                       m.TargetSquare.Name == "e4" || m.TargetSquare.Name == "e5" || m.TargetSquare.Name == "d4" || m.TargetSquare.Name == "d5")
-                .ThenBy(m => DevelopPiece(m.MovePieceType, board.PlyCount))
+                .ThenBy(m => DevelopPiece(m.MovePieceType, board))
+                .ThenByDescending(m => m.TargetSquare.Index == 27)
+                .ThenByDescending(m => m.TargetSquare.Index == 28)
+                .ThenByDescending(m => m.TargetSquare.Index == 20)
+                .ThenByDescending(m => m.TargetSquare.Index == 35)
+                .ThenByDescending(m => m.TargetSquare.Index == 36)
+                .ThenByDescending(m => m.TargetSquare.Index == 44)
             ;
 
 
@@ -63,13 +78,29 @@ public class MyBot : IChessBot
 
             foreach (var move in orderedMoves)
             {
-
                 board.MakeMove(move);
-                var value = AlphaBeta(board, depth - 1, maxDepth, !isMaximizer, !isWhite, alpha, beta, move);
+
+                _pieceCount = BitboardHelper.GetNumberOfSetBits(board.AllPiecesBitboard);//board.GetAllPieceLists().SelectMany(l => l).Count();
+               
+                var isInterestingMove = false;
+                if (board.IsInCheck() && maxDepth < 8)
+                {
+                    isInterestingMove = true;
+                    maxDepth += 2;
+                    depth += 2;
+                }
+
+                var value = AlphaBeta(board, depth - 1, maxDepth, !isMaximizer, !isWhite, alpha, beta);
 
                 //Console.WriteLine($"{move}: {value}");
 
                 board.UndoMove(move);
+                
+                if (isInterestingMove)
+                {
+                    maxDepth -= 2;
+                    depth -= 2;
+                }
 
                 if (best < value)
                 {
@@ -100,7 +131,7 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(move);
 
-                var value = AlphaBeta(board, depth - 1, maxDepth, !isMaximizer, !isWhite, alpha, beta, move);
+                var value = AlphaBeta(board, depth - 1, maxDepth, !isMaximizer, !isWhite, alpha, beta);
 
                 board.UndoMove(move);
 
@@ -118,25 +149,26 @@ public class MyBot : IChessBot
         }
     }
 
-    private int DevelopPiece(PieceType movePieceType, int plyCount)
+    
+    private int DevelopPiece(PieceType movePieceType, Board board)
     {
-
+        //return 0;
         switch (movePieceType)
         {
             case PieceType.None:
                 return 0;
             case PieceType.Pawn:
-                return plyCount < 8 ? 0 : 1000;
+                return board.PlyCount < 4 ? 0 : _rnd.Next(_pieceCount < 10 ? 0 : 50, 100);
             case PieceType.Knight:
-                return plyCount > 6 ? 10 : 80;
+                return board.PlyCount > 6 ? 10 : 80;
             case PieceType.Bishop:
-                return plyCount > 6 ? 20 : 90;
+                return board.PlyCount > 6 ? 20 : 90;
             case PieceType.Rook:
-                return plyCount > 40 ? 0 : 100;
+                return board.PlyCount > 20 ? 0 : 100;
             case PieceType.Queen:
-                return plyCount > 30 ? 0 : 100;
+                return board.PlyCount > 20 ? 0 : 100;
             case PieceType.King:
-                return plyCount > 60 ? 0 : 1000;
+                return board.PlyCount > 60 ? 0 : 1000;
             default:
                 throw new ArgumentOutOfRangeException(nameof(movePieceType), movePieceType, null);
         }
@@ -155,9 +187,10 @@ public class MyBot : IChessBot
 
         if (board.IsDraw())
         {
-            return diff < 0 
-                ? 10000 + 10000 / board.PlyCount 
-                : -10000 - (10000 / board.PlyCount);
+            //return diff < 0 
+            //    ? 10000 + 10000 / board.PlyCount 
+            //    : -10000 - (10000 / board.PlyCount);
+            return -10000 - (10000 / board.PlyCount);
         }
 
         if (board.IsInCheckmate())
@@ -197,6 +230,7 @@ public class MyBot : IChessBot
 
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     static readonly int[] PieceValues = { 0, 1, 3, 3, 5, 9, 10 };
+    private int _pieceCount;
 
     public static int GetPieceValue(PieceType pieceType)
     {
